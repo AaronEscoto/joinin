cdb5f175b6031a4fc8cba688274b5b95c1e44d13
alexung93 <alexung93@gmail.com>
2016-05-09 16:30:38 -0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.assets.compress = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   ################ FOR JAVASCRIPT HEROKU??
   #config.assets.compress = true
